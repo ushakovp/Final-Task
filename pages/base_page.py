4d1c513b3db05728dd6f16433536494b178e3c26
ushakovp <ushakovpb@gmail.com>
2019-07-22 19:44:23 +0400

    Rework Base page and Main page. Add some common tests

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,5 @@
 import math
+from .locators import BasePageLocators
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
@@ -50,3 +51,11 @@ class BasePage(object):
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+        #return LoginPage(browser=self.browser, url=self.browser.current_url) 
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
